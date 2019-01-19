330b61bc3a1036569505f63a400f3e357f1847a8
tedjenkins <29524044+tedjenkins@users.noreply.github.com>
2019-01-19 15:12:05 +0000

    Changes from helpful suggestions (err handling, no if block)

--- setup.sh
+++ setup.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 # Application generator script.
 read -p 'Name of directory: ' projectdir
-if [ -d "$projectdir" ] 
-then 
-  echo "Directory \"$projectdir\" exists, cancelling script"
-  exit 1
-  $SHELL
-fi
 git clone --depth=1 https://github.com/tedjenkins/generator.git "$projectdir"
+
 cd "$projectdir"
 echo "Removing and re-initialising git repository..."
 rm -rf .git
+
 git init
 git config --local user.name "George Pickering"
 git config --local user.email 29524044+tedjenkins@users.noreply.github.com
+
 echo "Done! You are now in project directory, remember to npm install!"
 $SHELL
