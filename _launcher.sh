d080c6e543fd7231f6c8a10c3ea941b89df2a123
tedjenkins <29524044+tedjenkins@users.noreply.github.com>
2019-02-06 10:17:53 +0000

    'Collection' (of scripts) structure finalised, user now has options

--- _launcher.sh
+++ _launcher.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 # Bash script launcher, providing easy command-line access to all scripts in this directory.
+SCRIPTLOCATION=$(dirname "$0");
 echo "Bash script launcher";
 echo "(c) 2019 George Pickering, MIT License";
-$SHELL;
+echo;
+echo "OPTIONS:";
+echo "(0) Debug";
+echo "(1) Project-setup (Jan 2019)";
+read -p "Enter the number and press [ENTER]: " ANSWER;
+if [ "$ANSWER" == 0 ]
+  then echo "Nothing to debug"
+elif [ "$ANSWER" == 1 ]
+  then "$SCRIPTLOCATION/project-setup.sh";
+  exit;
+else
+  echo "Invalid argument supplied"
+fi
+exec $SHELL;
--- project-setup.sh
+++ project-setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 # Application generator script.
-read -p 'Name of project: ' project
-git clone --depth=1 https://github.com/tedjenkins/generator.git "$project"
+read -p 'Name of project: ' project;
+git clone --depth=1 https://github.com/tedjenkins/generator.git "$project";
 
-cd "$project"
-echo "Removing and re-initialising git repository..."
-rm -rf .git
+cd "$project";
+echo "Removing and re-initialising git repository...";
+rm -rf .git;
 
-git init
-git config --local user.name "George Pickering"
-git config --local user.email 29524044+tedjenkins@users.noreply.github.com
+git init;
+git config --local user.name "George Pickering";
+git config --local user.email 29524044+tedjenkins@users.noreply.github.com;
 
-echo "Done! You are now in project directory, remember to npm install!"
-$SHELL
+echo "Done! You are now in project directory, remember to npm install!";
+exec $SHELL;
