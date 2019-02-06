5301b1cddfcd3908f540e426f2f20a47454ad7ff
tedjenkins <29524044+tedjenkins@users.noreply.github.com>
2019-02-06 11:31:10 +0000

    Node cmdline project setup added; scripts moved to /bin directory

--- _launcher.sh
+++ _launcher.sh
@@ -7,11 +7,15 @@ echo;
 echo "OPTIONS:";
 echo "(0) Debug";
 echo "(1) Project-setup (Jan 2019)";
+echo "(2) Node-cmdline-project-setup (Node.js command-line scripting)";
 read -p "Enter the number and press [ENTER]: " ANSWER;
-if [ "$ANSWER" == 0 ]
+if [ $ANSWER == 0 ]
   then echo "Nothing to debug"
-elif [ "$ANSWER" == 1 ]
-  then "$SCRIPTLOCATION/project-setup.sh";
+elif [ $ANSWER == 1 ]
+  then "$SCRIPTLOCATION/bin/project-setup.sh";
+  exit;
+elif [ $ANSWER == 2 ]
+  then "$SCRIPTLOCATION/bin/node-cmdline-project-setup.sh";
   exit;
 else
   echo "Invalid argument supplied"
---
+++ bin/node-cmdline-project-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+# Application generator script for command-line applications running on Node.js.
+if ! [ -x "$(command -v node)" ]
+  then echo "ERROR: Node does not exist or cannot be recognised on this machine.";
+else
+  read -p 'Name of project: ' project;
+  git clone --depth=1 https://github.com/tedjenkins/node-cmdline-project-generator.git "$project";
+
+  cd "$project";
+  echo "Removing and re-initialising git repository...";
+  rm -rf .git;
+
+  git init;
+  git config --local user.name "George Pickering";
+  git config --local user.email 29524044+tedjenkins@users.noreply.github.com;
+
+  echo "Done! You are now in project directory, remember to npm install!";
+fi
+
+exec $SHELL;
